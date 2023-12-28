74fa6e5135c5e9b65e1851ed8d462b4ded801ad7
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-12-28 00:04:20 +0000

    Final updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import Slider from './lib/Slider.svelte';
 	import Checkbox from './lib/Checkbox.svelte';
 
@@ -61,7 +62,6 @@
 	}
 
 	function styleBars(numBars) {
-		
 		resetBars();
 		
 		let barColor;
@@ -96,6 +96,11 @@
 		if (!canCopy) return;
 		await navigator.clipboard.writeText(password);
 		copied = true;
+
+		// remove copied notification after 2 seconds
+		setTimeout(() => {
+			copied = false;
+		}, 2000);
 	}
 
 </script>
@@ -106,7 +111,9 @@
 	<div class="password-output">
 		<span class:canCopy class="password">{password}</span>
 		<div class="copy-container">
-			<span class:copied class="copy-confirm">Copied</span>
+			{#if copied}
+				<span out:fade={{duration: 500}} class="copy-confirm">Copied</span>
+			{/if}
 			<button type="button" on:click={copyPassword}>
 				<svg class="copy-icon" width="21" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z" fill="currentColor"/></svg>
 			</button>
@@ -185,18 +192,19 @@
 	.copy-container {
 		display: flex;
 		align-items: center;
+		position: relative;
 	}
 	
 	.copy-confirm {
 		font-size: 18px;
+		line-height: 32px;
 		color: var(--neon-green);
 		text-transform: uppercase;
 		margin-right: 16px;
-		display: none;
-	}
-
-	.copy-confirm.copied {
-		display: inline;
+		position: absolute;
+		left: -80px;
+		padding: 0 10px;
+		background: var(--dark-grey);
 	}
 
 	.copy-icon {
